04e7b1a2169d32fc1ed3556da0a598cdc805fe81
Jonas-BJunior <jonassbsouza@gmail.com>
2026-03-26 15:07:12 -0300

    fix tests

---
+++ CollectionProject/CollectionProjectUITests/CollectionProjectUITests.swift
@@ -0,0 +1,23 @@
+import XCTest
+
+class CollectionProjectUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that it tests.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
+    }
+}
